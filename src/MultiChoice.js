4ca1352074943a432ad1072d701b3e549e14d756
Paul Perkins <paulperkins7@gmail.com>
2019-11-30 21:04:50 -0600

    add grid to multiChoice

--- src/MultiChoice.js
+++ src/MultiChoice.js
@@ -5,21 +5,27 @@ import {
 
 const MultiChoice = (props) => {
     return (
-        <div>
-            <div>
-                <img src={props.correctDog.dogImg} alt={props.correctDog.dogName}/>
-            </div>
-            {props.dogOptions.map((value, index) => {
-              return (
-                <div 
-                    className="btn btn-secondary m-3" 
-                    key={index}
-                    onClick={() => props.checkAnswer(value.dogName)}
-                >
-                {value.dogName} 
+        <div className="container">
+            <div className="row">
+            <div className="col-6">
+                <div>
+                    <img src={props.correctDog.dogImg} alt={props.correctDog.dogName} className="w-100" />
                 </div>
-              )
-            })}
+                </div>
+                <div className="col-6">
+                {props.dogOptions.map((value, index) => {
+                return (
+                    <div 
+                        className="btn btn-primary m-3" 
+                        key={index}
+                        onClick={() => props.checkAnswer(value.dogName)}
+                    >
+                    {value.dogName} 
+                    </div>
+                )
+                })}
+                </div>
+            </div>
         </div>
     
       );
