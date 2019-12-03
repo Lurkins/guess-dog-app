979a71622e14a8262edd58537fff8c675f7caea6
Paul Perkins <paulperkins7@gmail.com>
2019-12-02 22:35:54 -0600

    try with shorter header

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,21 +3,21 @@ import { UncontrolledCarousel } from 'reactstrap';
 
 const items = [
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/0/06/Askals_or_aspins_are_mongrel_dogs_in_the_Philippines.jpg',
+    src: 'https://i0.wp.com/allislandpetsupplies.com/wp-content/uploads/2018/12/bernese-mountain-dog-in-swiss-alps-mountain-web-header.jpg?w=1280&ssl=1',
     altText: 'Slide 1',
     caption: 'Slide 1',
     header: 'Slide 1 Header',
     key: '1'
   },
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/0/06/Askals_or_aspins_are_mongrel_dogs_in_the_Philippines.jpg',
+    src: 'https://i0.wp.com/allislandpetsupplies.com/wp-content/uploads/2018/12/bernese-mountain-dog-in-swiss-alps-mountain-web-header.jpg?w=1280&ssl=1',
     altText: 'Slide 2',
     caption: 'Slide 2',
     header: 'Slide 2 Header',
     key: '2'
   },
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/0/06/Askals_or_aspins_are_mongrel_dogs_in_the_Philippines.jpg',
+    src: 'https://i0.wp.com/allislandpetsupplies.com/wp-content/uploads/2018/12/bernese-mountain-dog-in-swiss-alps-mountain-web-header.jpg?w=1280&ssl=1',
     altText: 'Slide 3',
     caption: 'Slide 3',
     header: 'Slide 3 Header',
