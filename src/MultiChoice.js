5fca3b07d98a128c7ab705b0e43738f69db965c2
Paul Perkins <paulperkins7@gmail.com>
2019-12-05 22:46:32 -0600

    fix quiz layout

--- src/MultiChoice.js
+++ src/MultiChoice.js
@@ -2,64 +2,66 @@ import React from 'react';
 import {
 
 } from 'reactstrap'; 
+import './MultiChoice.css';
 
 const MultiChoice = (props) => {
     return (
-        <div className="mt-5 vh-25">
-        
-        <div className="container">
-            <div className="row">
-            <div className="col-md-6 col-12">
-                <div className="h-50">
-                    <img src={props.correctDog.dogImg} alt={props.correctDog.dogName} className="w-100 rounded" />
-                </div>
-                </div>
-                <div className="col-md-6 col-12">
-                    <div className="row">
-                        <div className="col-6">
-                        {(() => {
-                        switch(props.correctAnswer) {
-                        case true:
-                            return <div className="text-success display-4 text-center">Correct Answer!</div>
-                        case false:
-                            return <div className="text-danger display-4 text-center">Wrong Answer!</div> 
-                        default:
-                            return <div className="text-danger display-4"></div> 
-                        }
-                    })()}
-                            <div className="w-100 display-4">
-                                <p>Score: {props.currentScore}</p>
-                            </div>
+        <div className="vh-50">
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-6">
+                        <div className="w-100 score-text">
+                            <p>Score: {props.currentScore}</p>
                         </div>
-                        <div className="col-6">
-                            <div className="w-100 h-100 display-4 d-flex justify-content-center align-items-center">
-                                <div className="btn btn-warning d-block" onClick={props.resetScore}>
-                                    Reset Score
-                                </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="w-100 h-100 display-4 d-flex justify-content-end align-items-center">
+                            <div className="btn btn-warning d-block mb-2" onClick={props.resetScore}>
+                                Reset Score
                             </div>
                         </div>
                     </div>
-                    {props.dogOptions.map((value, index) => {
-                    return (
+                </div>
+                <div className="row">
+                <div className="col-md-6 col-12">
+                    <div className="vh-50 d-flex justify-content-center">
+                        <img src={props.correctDog.dogImg} alt={props.correctDog.dogName} className="rounded multichoice-dog " />
+                    </div>
+                    <div className="answer-area">
+                        {(() => {
+                            switch(props.correctAnswer) {
+                            case true:
+                                return <div className="text-success display-4 text-center">Correct Answer!</div>
+                            case false:
+                                return <div className="text-danger display-4 text-center">Wrong Answer!</div> 
+                            default:
+                                return <div className="text-danger display-4"></div> 
+                            }
+                        })()}
+                    </div>
+                    </div>
+                    <div className="col-md-6 col-12">
+                        {props.dogOptions.map((value, index) => {
+                        return (
+                            <div 
+                                className="btn btn-primary my-1 w-100" 
+                                key={index}
+                                onClick={() => props.checkAnswer(value.dogName)}
+                            >
+                                {value.dogName} 
+                            </div>
+                        )
+                        })}
                         <div 
-                            className="btn btn-primary my-1 w-100" 
-                            key={index}
-                            onClick={() => props.checkAnswer(value.dogName)}
+                            className="btn btn-success my-3 w-100" 
+                            onClick={props.getDogOptionsArray}
                         >
-                            {value.dogName} 
+                            NEXT DOG
                         </div>
-                    )
-                    })}
-                    <div 
-                        className="btn btn-success my-3 w-100" 
-                        onClick={props.getDogOptionsArray}
-                    >
-                        NEXT DOG
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 }
 
