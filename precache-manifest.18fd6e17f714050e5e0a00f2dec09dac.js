eefe7775a6cfa67cf241ab78d8d4fd766b2a6440
Paul Perkins <paulperkins7@gmail.com>
2020-01-04 17:58:13 -0600

    Updates

---
+++ precache-manifest.18fd6e17f714050e5e0a00f2dec09dac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4312d1053af30a8acad0beff6edc8536",
+    "url": "/guess-dog-app/index.html"
+  },
+  {
+    "revision": "04ae520e70a0de11af81",
+    "url": "/guess-dog-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a0b329f6f5eb691362f4",
+    "url": "/guess-dog-app/static/css/main.ab3cf920.chunk.css"
+  },
+  {
+    "revision": "04ae520e70a0de11af81",
+    "url": "/guess-dog-app/static/js/2.522221f9.chunk.js"
+  },
+  {
+    "revision": "a7450c05cc607478b653743cc277b6ce",
+    "url": "/guess-dog-app/static/js/2.522221f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0b329f6f5eb691362f4",
+    "url": "/guess-dog-app/static/js/main.206084a6.chunk.js"
+  },
+  {
+    "revision": "8f370d930328a3bad55c",
+    "url": "/guess-dog-app/static/js/runtime-main.2af75a1c.js"
+  },
+  {
+    "revision": "b74e25b99fed53f52e6245aefc226ee5",
+    "url": "/guess-dog-app/static/media/home-paw.b74e25b9.jpg"
+  }
+]);
